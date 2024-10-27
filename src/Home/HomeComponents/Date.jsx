b9f221ef3835c68fc874fb2f6e0047881b1540c4
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 02:18:49 +0600

    Date component created

---
+++ src/Home/HomeComponents/Date.jsx
@@ -0,0 +1,42 @@
+
+
+const Date = () => {
+    return (
+        <div>
+             <div className="navbar bg-base-100  rounded-lg border">
+                <div className="flex-1">
+                    <div className="form-control relative">
+                        <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                        <input
+                            type="text"
+                            placeholder="Search here..."
+                            className="input input-bordered pl-10 w-24 md:w-auto"
+                        />
+                    </div>
+                </div>
+                <div className="flex-none gap-0 md:gap-2">
+                    <div className="flex items-center gap-2">
+                        <div className="flex gap-6 md:gap-8">
+                            <div>
+                                <div className="indicator">
+                                    <span className="indicator-item w-6   text-center bg-red-700 text-white rounded-full">1</span>
+                                    <div className="text-2xl" ><HiOutlineBell /></div>
+                                </div>
+                            </div>
+                            <div className="text-2xl">
+                                <AiOutlineMessage />
+                            </div>
+                        </div>
+                        <div className="divider divider-horizontal"></div>
+                        <div className="hidden md:block text-right">
+                            <p className="font-bold">Nayamul Roni</p>
+                            <p className="gray text-sm">Super Admin</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Date;
