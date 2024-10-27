6be2e53220adaa21bc268cae64c0d94f02b389b6
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 03:44:12 +0600

    react icons added for each item

--- src/Home/HomeComponents/Statistics.jsx
+++ src/Home/HomeComponents/Statistics.jsx
@@ -1,4 +1,4 @@
-import { HiOutlineUser ,MdOutlineFeaturedPlayList } from "react-icons/hi2";
+import { HiOutlineUser ,MdOutlineFeaturedPlayList, GiKnifeFork,  RiMotorbikeLine, PiMotorcycleFill, FaMoneyBillWave} from "react-icons/hi2";
 
 const Statistics = () => {
     const items = [
@@ -6,10 +6,10 @@ const Statistics = () => {
         { id: 2, icon: <MdOutlineFeaturedPlayList />, title: 'Total Orders', number: 10457 },
         { id: 3, icon: <MdOutlineFeaturedPlayList />, title: 'Completed Orders', number: 3000 },
         { id: 4, icon: <MdOutlineFeaturedPlayList />, title: 'Cancelled Orders', number: 240 },
-        { id: 5, icon: <FaLeaf />, title: 'Restaurant Partners', number: 1200 },
-        { id: 6, icon: <FaPlane />, title: 'Rider Partners', number: 3628 },
-        { id: 7, icon: <FaHeart />, title: 'Approved Rider', number: 17349 },
-        { id: 8, icon: <FaBook />, title: 'Coupons Active', number: 1393 },
+        { id: 5, icon: <GiKnifeFork />, title: 'Restaurant Partners', number: 1200 },
+        { id: 6, icon: <RiMotorbikeLine />, title: 'Rider Partners', number: 3628 },
+        { id: 7, icon: <PiMotorcycleFill />, title: 'Approved Rider', number: 17349 },
+        { id: 8, icon: <FaMoneyBillWave />, title: 'Coupons Active', number: 1393 },
     ];
     return (
         <div className="grid  grid-cols-1 md:grid-cols-4 gap-6">
