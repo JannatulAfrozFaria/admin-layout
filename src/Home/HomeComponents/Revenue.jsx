8cccba73d663fd016d09a6afadb90ef8aabecebe
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 04:36:36 +0600

    3dots icon added

--- src/Home/HomeComponents/Revenue.jsx
+++ src/Home/HomeComponents/Revenue.jsx
@@ -1,11 +1,11 @@
 
-
+import { BsThreeDotsVertical } from "react-icons/bs";
 const Revenue = () => {
     return (
         <div>
             <div className="flex justify-between">
                 <div className="dropdown dropdown-bottom dropdown-end">
-                    <div tabIndex={0} role="button" className="btn m-1">Click</div>
+                    <div tabIndex={0} role="button" className="btn m-1"><BsThreeDotsVertical /></div>
                     <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                         <li><a>Details</a></li>
                     </ul>
