ec5a3e97ed5f9feee2e923b1e03131b73f530046
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 05:05:32 +0600

    Order Summary Component created

---
+++ src/Home/HomeComponents/OrderSummary.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../UtilityComponents/SectionTitle";
+
+
+const OrderSummary = () => {
+    return (
+        <div>
+            <SectionTitle title={'Revenue 600 AED'}></SectionTitle>
+        </div>
+    );
+};
+
+export default OrderSummary;
