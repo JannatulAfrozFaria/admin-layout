ec32e72ce98b862be87bf79daad072368e952e0c
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 02:30:42 +0600

    contents added to the left side

--- src/Home/HomeComponents/Date.jsx
+++ src/Home/HomeComponents/Date.jsx
@@ -3,36 +3,20 @@
 const Date = () => {
     return (
         <div>
-             <div className="navbar bg-base-100  rounded-lg border">
-                <div className="flex-1">
-                    <div className="form-control relative">
-                        <CiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                        <input
-                            type="text"
-                            placeholder="Search here..."
-                            className="input input-bordered pl-10 w-24 md:w-auto"
-                        />
+            <div className=" bg-base-100">
+                <div className="flex-1 navbar customDiv">
+                    <div className="join">
+                        <input className="join-item btn" type="radio" name="options" aria-label="Radio 1" />
+                        <input className="join-item btn" type="radio" name="options" aria-label="Radio 2" />
+                        <input className="join-item btn" type="radio" name="options" aria-label="Radio 3" />
                     </div>
                 </div>
                 <div className="flex-none gap-0 md:gap-2">
-                    <div className="flex items-center gap-2">
-                        <div className="flex gap-6 md:gap-8">
-                            <div>
-                                <div className="indicator">
-                                    <span className="indicator-item w-6   text-center bg-red-700 text-white rounded-full">1</span>
-                                    <div className="text-2xl" ><HiOutlineBell /></div>
-                                </div>
-                            </div>
-                            <div className="text-2xl">
-                                <AiOutlineMessage />
-                            </div>
-                        </div>
-                        <div className="flex gap-2 navbar customDiv ">
-                           <div>
+                    <div className="flex items-center gap-2 navbar customDiv ">
+                        <div>
                             <img src="https://i.postimg.cc/gJwd0ywd/date.png" alt="calender-logo" />
-                           </div>
-                            <p className="font-semibold ">Custom Date</p>
                         </div>
+                        <p className="font-semibold ">Custom Date</p>
                     </div>
                 </div>
             </div>
