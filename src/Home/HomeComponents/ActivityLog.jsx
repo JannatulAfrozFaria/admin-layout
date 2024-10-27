a55293878dcc308c1eb15caaa68111c19c734504
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 05:13:47 +0600

    name property added to each item of activity array

--- src/Home/HomeComponents/ActivityLog.jsx
+++ src/Home/HomeComponents/ActivityLog.jsx
@@ -3,11 +3,11 @@ import SectionTitle from "../../UtilityComponents/SectionTitle";
 
 const ActivityLog = () => {
     const Activities = [
-        { id: 1, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
-        { id: 2, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
-        { id: 3, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
-        { id: 4, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
-        { id: 5, date:'12-04-2024', time:'10:14 PM', title: 'New items approved', number: 10 },
+        { id: 1, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', name: 'Jakit Sarker' },
+        { id: 2, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', name: 'Jakit Sarker' },
+        { id: 3, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', name: 'Jakit Sarker' },
+        { id: 4, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', name: 'Jakit Sarker' },
+        { id: 5, date:'12-04-2024', time:'10:14 PM', title: 'New items approved', name: 'Jakit Sarker' },
     ];
     return (
         <div>
