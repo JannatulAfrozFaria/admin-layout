a4cc60e26f7eaa49c50dd0c884838a5f6b8c5566
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 03:47:39 +0600

    statistics array mapped for ui

--- src/Home/HomeComponents/Statistics.jsx
+++ src/Home/HomeComponents/Statistics.jsx
@@ -1,4 +1,4 @@
-import { HiOutlineUser ,MdOutlineFeaturedPlayList, GiKnifeFork,  RiMotorbikeLine, PiMotorcycleFill, FaMoneyBillWave} from "react-icons/hi2";
+import { HiOutlineUser ,MdOutlineFeaturedPlayList, GiKnifeFork,  RiMotorbikeLine, PiMotorcycleFill, FaMoneyBillWave, LiaMoneyBillWaveSolid } from "react-icons/hi2";
 
 const Statistics = () => {
     const items = [
@@ -9,11 +9,22 @@ const Statistics = () => {
         { id: 5, icon: <GiKnifeFork />, title: 'Restaurant Partners', number: 1200 },
         { id: 6, icon: <RiMotorbikeLine />, title: 'Rider Partners', number: 3628 },
         { id: 7, icon: <PiMotorcycleFill />, title: 'Approved Rider', number: 17349 },
-        { id: 8, icon: <FaMoneyBillWave />, title: 'Coupons Active', number: 1393 },
+        { id: 8, icon: <LiaMoneyBillWaveSolid />, title: 'Coupons Active', number: 1393 },
     ];
     return (
         <div className="grid  grid-cols-1 md:grid-cols-4 gap-6">
-            
+            {items.map(item => (
+                <div key={item.id} className="flex items-center p-4 border rounded-lg">
+                    <div>
+                        <span>{item.icon}</span>
+                    </div>
+                    
+                    <div className="ml-2">
+                        <h4 className="font-semibold">{item.title}</h4>
+                        <p>{item.number}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
