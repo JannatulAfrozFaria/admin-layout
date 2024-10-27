4dde9048f295aa607251271b6db63a80dafb50bd
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 05:08:24 +0600

    array created for activity log

--- src/Home/HomeComponents/ActivityLog.jsx
+++ src/Home/HomeComponents/ActivityLog.jsx
@@ -2,6 +2,13 @@ import SectionTitle from "../../UtilityComponents/SectionTitle";
 
 
 const ActivityLog = () => {
+    const Activities = [
+        { id: 1, title: 'Restaurant Account', number: 10 },
+        { id: 2, title: 'Restaurant Onboarding', number: 10 },
+        { id: 3, title: 'Rider Partners', number: 10 },
+        { id: 4, title: 'Rider Onboarding', number: 10 },
+        { id: 5, title: 'Support Tickets', number: 10 },
+    ];
     return (
         <div>
             <SectionTitle title={'Activity Log'}></SectionTitle>
