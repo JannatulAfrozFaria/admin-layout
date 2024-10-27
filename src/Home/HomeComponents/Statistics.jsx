2e77bc360e88f66ce4cfebe6f57f3ac2f5f9d79b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 03:27:57 +0600

    array created for statistics info

--- src/Home/HomeComponents/Statistics.jsx
+++ src/Home/HomeComponents/Statistics.jsx
@@ -1,6 +1,16 @@
-
+import { HiOutlineUser } from "react-icons/hi2";
 
 const Statistics = () => {
+    const items = [
+        { id: 1, icon: <HiOutlineUser />, title: 'Total Customer', number: 120 },
+        { id: 2, icon: <FaCoffee />, title: 'Coffee', number: 85 },
+        { id: 3, icon: <FaApple />, title: 'Apples', number: 30 },
+        { id: 4, icon: <FaCar />, title: 'Cars', number: 40 },
+        { id: 5, icon: <FaLeaf />, title: 'Leaves', number: 70 },
+        { id: 6, icon: <FaPlane />, title: 'Planes', number: 10 },
+        { id: 7, icon: <FaHeart />, title: 'Hearts', number: 200 },
+        { id: 8, icon: <FaBook />, title: 'Books', number: 50 },
+    ];
     return (
         <div className="grid  grid-cols-1 md:grid-cols-4 gap-6">
             
