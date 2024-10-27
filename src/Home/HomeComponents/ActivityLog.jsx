47d1fd27765c9df4a9b832183ab7acb72d62ebca
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 05:12:23 +0600

    title added to activity array

--- src/Home/HomeComponents/ActivityLog.jsx
+++ src/Home/HomeComponents/ActivityLog.jsx
@@ -3,11 +3,11 @@ import SectionTitle from "../../UtilityComponents/SectionTitle";
 
 const ActivityLog = () => {
     const Activities = [
-        { id: 1, date:'12-04-2024', time:'10:54 PM', title: 'Restaurant Account', number: 10 },
-        { id: 2, date:'12-04-2024', time:'10:54 PM', title: 'Restaurant Onboarding', number: 10 },
-        { id: 3, date:'12-04-2024', time:'10:54 PM', title: 'Rider Partners', number: 10 },
-        { id: 4, date:'12-04-2024', time:'10:54 PM', title: 'Rider Onboarding', number: 10 },
-        { id: 5, date:'12-04-2024', time:'10:54 PM', title: 'Support Tickets', number: 10 },
+        { id: 1, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
+        { id: 2, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
+        { id: 3, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
+        { id: 4, date:'12-04-2024', time:'10:14 PM', title: 'Modified new item', number: 10 },
+        { id: 5, date:'12-04-2024', time:'10:14 PM', title: 'New items approved', number: 10 },
     ];
     return (
         <div>
