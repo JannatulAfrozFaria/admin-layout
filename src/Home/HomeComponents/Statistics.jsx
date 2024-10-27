60daf4befc7b87a4cb25d8c0ec63c78dd3f2beb6
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 03:36:04 +0600

    Text and Number info updated in statistics array

--- src/Home/HomeComponents/Statistics.jsx
+++ src/Home/HomeComponents/Statistics.jsx
@@ -1,15 +1,15 @@
-import { HiOutlineUser } from "react-icons/hi2";
+import { HiOutlineUser ,MdOutlineFeaturedPlayList } from "react-icons/hi2";
 
 const Statistics = () => {
     const items = [
-        { id: 1, icon: <HiOutlineUser />, title: 'Total Customer', number: 120 },
-        { id: 2, icon: <FaCoffee />, title: 'Coffee', number: 85 },
-        { id: 3, icon: <FaApple />, title: 'Apples', number: 30 },
-        { id: 4, icon: <FaCar />, title: 'Cars', number: 40 },
-        { id: 5, icon: <FaLeaf />, title: 'Leaves', number: 70 },
-        { id: 6, icon: <FaPlane />, title: 'Planes', number: 10 },
-        { id: 7, icon: <FaHeart />, title: 'Hearts', number: 200 },
-        { id: 8, icon: <FaBook />, title: 'Books', number: 50 },
+        { id: 1, icon: <HiOutlineUser />, title: 'Total Customer', number: 3301 },
+        { id: 2, icon: <MdOutlineFeaturedPlayList />, title: 'Total Orders', number: 10457 },
+        { id: 3, icon: <MdOutlineFeaturedPlayList />, title: 'Completed Orders', number: 3000 },
+        { id: 4, icon: <MdOutlineFeaturedPlayList />, title: 'Cancelled Orders', number: 240 },
+        { id: 5, icon: <FaLeaf />, title: 'Restaurant Partners', number: 1200 },
+        { id: 6, icon: <FaPlane />, title: 'Rider Partners', number: 3628 },
+        { id: 7, icon: <FaHeart />, title: 'Approved Rider', number: 17349 },
+        { id: 8, icon: <FaBook />, title: 'Coupons Active', number: 1393 },
     ];
     return (
         <div className="grid  grid-cols-1 md:grid-cols-4 gap-6">
