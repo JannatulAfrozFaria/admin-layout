f8e89934a6ad196964a5fcd34f1dde7303e535bf
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 05:10:28 +0600

    time and date added to activity array

--- src/Home/HomeComponents/ActivityLog.jsx
+++ src/Home/HomeComponents/ActivityLog.jsx
@@ -3,11 +3,11 @@ import SectionTitle from "../../UtilityComponents/SectionTitle";
 
 const ActivityLog = () => {
     const Activities = [
-        { id: 1, title: 'Restaurant Account', number: 10 },
-        { id: 2, title: 'Restaurant Onboarding', number: 10 },
-        { id: 3, title: 'Rider Partners', number: 10 },
-        { id: 4, title: 'Rider Onboarding', number: 10 },
-        { id: 5, title: 'Support Tickets', number: 10 },
+        { id: 1, date:'12-04-2024', time:'10:54 PM', title: 'Restaurant Account', number: 10 },
+        { id: 2, date:'12-04-2024', time:'10:54 PM', title: 'Restaurant Onboarding', number: 10 },
+        { id: 3, date:'12-04-2024', time:'10:54 PM', title: 'Rider Partners', number: 10 },
+        { id: 4, date:'12-04-2024', time:'10:54 PM', title: 'Rider Onboarding', number: 10 },
+        { id: 5, date:'12-04-2024', time:'10:54 PM', title: 'Support Tickets', number: 10 },
     ];
     return (
         <div>
