e0e1ee80f731486f16de06690d903d59c7deb08a
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 06:34:21 +0600

    progress bar added in chart section

--- src/Home/HomeComponents/OrderSummary.jsx
+++ src/Home/HomeComponents/OrderSummary.jsx
@@ -3,10 +3,10 @@ import { FiArchive } from "react-icons/fi";
 
 const OrderSummary = () => {
     const OrderStats = [
-        { id: 1, title: 'Active', number: 389 },
-        { id: 2, title: 'Pending', number: 125 },
-        { id: 3, title: 'Refunded', number: 234 },
-        { id: 4, title: 'Cancelled', number: 234 },
+        { id: 1, title: 'Active', number: 389, percentage: 43, value: 50 },
+        { id: 2, title: 'Pending', number: 125, percentage: 43, value: 50 },
+        { id: 3, title: 'Refunded', number: 234, percentage: 43, value: 50 },
+        { id: 4, title: 'Cancelled', number: 234, percentage: 43, value: 50 },
     ];
     return (
         <div className="customDiv2"
@@ -24,7 +24,7 @@ const OrderSummary = () => {
             </div>
             {/* ORDER-DATA------- */}
             <div className="grid grid-cols-4 gap-4">
-            {OrderStats.map(item => (
+                {OrderStats.map(item => (
                     <div key={item.id} className="flex flex-col gap-2 justify-center py-2 px-5  border rounded-lg bg-base-100 ">
                         <p className="text-2xl">{item.number}</p>
                         <p className="gray">{item.title}</p>
@@ -34,7 +34,11 @@ const OrderSummary = () => {
             {/* PI-------CHART */}
             <div className="my-4 grid grid-cols-2 gap-4">
                 <div></div>
-                
+                <div className="grid grid-cols-1 gap-4">
+                    {OrderStats.map(item => (
+                            <progress key={item.id} className="progress progress-success w-56" value={0} max="100"></progress>
+                    ))}
+                </div>
             </div>
         </div>
     );
