403b4af9b0b128660db219d5169942d042892e80
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-28 06:10:10 +0600

    archive icon added

--- src/Home/HomeComponents/OrderSummary.jsx
+++ src/Home/HomeComponents/OrderSummary.jsx
@@ -1,5 +1,5 @@
 import SectionTitle from "../../UtilityComponents/SectionTitle";
-
+import { FiArchive } from "react-icons/fi";
 
 const OrderSummary = () => {
     return (
@@ -8,7 +8,7 @@ const OrderSummary = () => {
             <SectionTitle title={'Current Order Summary'}></SectionTitle>
             <div className="pendingNumber flex justify-between ">
                 <div className="flex gap-4">
-                    <div></div>
+                    <div> <FiArchive /></div>
                     <div>125 new Orders!</div>
                 </div>
 
